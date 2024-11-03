b6bb4c7ce990cd4a47769c7c6c7143a6a80ee81d
janmenjayap <janmenjaya.panda.22@gmail.com>
2024-11-03 18:24:24 +0530

    updated the project name

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'gsoc-2024'
+project = ''
 copyright = '2024, Janmenjaya Panda'
 author = 'Janmenjaya Panda'
 
